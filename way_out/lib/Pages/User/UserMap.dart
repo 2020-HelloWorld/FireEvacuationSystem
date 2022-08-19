4acb3d8fd49d87ec202d79cfe6c07283e38079d5
Ghanashyam-Bhat <ghanashyambhat6@gmail.com>
2022-08-19 19:09:08 +0530

    Firebase connection

---
+++ way_out/lib/Pages/User/UserMap.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserMap extends StatefulWidget {
+  const UserMap({Key? key}) : super(key: key);
+
+  @override
+  State<UserMap> createState() => _UserMapState();
+}
+
+class _UserMapState extends State<UserMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- way_out/lib/main.dart
+++ way_out/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:way_out/Pages/SplashScreen.dart';
-
-void main(){
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
